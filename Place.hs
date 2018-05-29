dc1f0fdbd41368898638eff29f2dde6fa7d821ff
mynguyenbmc <mnguyen1@brynmawr.edu>
2018-05-29 13:18:05 -0400

    place data types

---
+++ Place.hs
@@ -0,0 +1,22 @@
+module Place where
+
+data Place = Place { zipCodeP :: String
+                    , townP :: String
+                    , stateP :: String }
+                    deriving (Eq, Show, Read, Ord)
+
+data LocatedPlace = LocatedPlace { zipCodeLP :: String
+                                  , townLP :: String
+                                  , stateLP :: String
+                                  , latLP :: Float
+                                  , lonLP :: Float }
+                                  deriving (Eq, Show, Read, Ord)
+
+data PopulatedPlace = PopulatedPlace { zipCodePP :: String
+                                  , townPP :: String
+                                  , statePP :: String
+                                  , latPP :: Float
+                                  , lonPP :: Float
+                                  , populationPP :: Int }
+                                  deriving (Eq, Show, Read, Ord)
+                                
