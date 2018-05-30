cd561ddb2afa9819a425ed2c6d13f85810979017
mynguyenbmc <mnguyen1@brynmawr.edu>
2018-05-30 14:56:42 -0400

    trivial

--- Parse.hs
+++
@@ -1,8 +0,0 @@
-module Parse where
-
-sepBy :: Char -> String -> [String]
-sepBy _ [] = []
-sepBy d l@(x:xs)
-   | x == d = sepBy d xs
-   | otherwise = px : sepBy d sx
-      where (px, sx) = break (== d) l
