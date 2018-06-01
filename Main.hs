4deaf7745fc1dbf0d0ce09f414d5d46b6fbc6ca6
mynguyenbmc <mnguyen1@brynmawr.edu>
2018-06-01 00:13:35 -0400

    optimized

--- Main.hs
+++ Main.hs
@@ -1,83 +1,35 @@
 module Main where
-import Class
-import Place
-import Data.Csv
---import qualified Data.ByteString.Lazy as BL
-import Prelude as P
-import qualified Data.Vector as V
-import Data.HashMap.Strict as H
 import System.IO
+import Data.HashMap.Strict as H
+import Data.List
+import System.Random as R
 
-locationFile = "ziplocs.csv"
-populationFile = "uszipcodes.csv"
+insertBigrams :: [String] -> HashMap String [String]-> HashMap String [String]
+insertBigrams [] h = h
+insertBigrams (x : []) h = h
+insertBigrams (x : rest@(x' : xs)) h = insertBigrams rest $ H.insertWith f x [x'] h
+   where f a b = a ++ b
 
-sepBy :: Char -> String -> [String]
-sepBy _ [] = []
-sepBy d l@(x:xs)
-   | (x == d || x == '\r') = sepBy d xs
-   | otherwise = px : sepBy d sx
-      where (px, sx) = break (== d) l
+printText :: (RandomGen g) => Int -> g -> String -> HashMap String [String] -> IO ()
+printText 0 _ _ _ = putStr "\n"
+printText n g k h = case H.lookup k h of
+  Nothing -> error "invalid input"
+  Just v -> do
+             putStr (k ++ " ")
+             printText (n-1) nextG nextK h
+                where (nextK, nextG) = pick v g
 
-
-getPopulated :: [String] -> Maybe Place
-getPopulated [] = Nothing
-getPopulated l@(x:xs)
-  | length l <= 3 = Just $ Place zipcode town state
-  | otherwise = Just $ PopulatedPlace zipcode town state (-1.0) (-1.0) (read $ l !! 3 :: Int)
-  where
-    zipcode = l !! 0
-    town = l !! 1
-    state = l !! 2
-
-getObjects :: [String] -> HashMap String Place -> HashMap String Place--[Maybe Place]
-getObjects [] h = h
-getObjects (x : xs) h = getObjects xs $ H.insert (obj !! 0) (extract $ getPopulated obj) h
-  where
-    obj = sepBy ',' x
-    extract :: Maybe Place -> Place
-    extract (Just p) = p
-
-newLocatedPlace :: Float -> Float -> Place -> Place
-newLocatedPlace lat long (Place z t s) = LocatedPlace z t s lat long
-newLocatedPlace lat long (PopulatedPlace z t s _ _ popu) = PopulatedPlace z t s lat long popu
-
-getLocated :: HashMap String Place -> [String] -> Maybe Place
-getLocated _ [] = Nothing
-getLocated h l@(x : xs)
-  | H.member x h == False = if length(l) <= 3 then Just $ Place zipcode town state
-      else Just $ LocatedPlace zipcode town state lat long
-  | otherwise = Just $ newLocatedPlace lat long $ extract oldv
-  where
-    oldv = H.lookup x h
-    zipcode = l !! 0
-    town = l !! 2
-    state = l !! 3
-    lat = read $ l !! 5 :: Float
-    long = read $ l !! 6 :: Float
-    extract :: Maybe Place -> Place
-    extract (Just p) = p
-
-finalMap :: HashMap String Place -> [[String]] -> HashMap String Place
-finalMap h [] = h
-finalMap h (x : xs) = finalMap (H.insert (head x) (extract $ getLocated h x) h) xs
-  where
-    extract :: Maybe Place -> Place
-    extract (Just p) = p
+pick :: (RandomGen g) => [a] -> g -> (a,g)
+pick xs g = (xs !! a, ng)
+   where (a,ng) = R.randomR (0, length xs - 1) g
 
 main = do
-  population <- readFile populationFile
-  let plines = tail $ lines population
-  let populationPlaces = getObjects plines populationMap
-  location <- readFile locationFile
-  let llines = P.map (sepBy ',') $ tail $ lines location
-  let headlen = length $ head llines
-  --let places = P.map (\x -> H.insert (head x) (extract $ getLocated populationPlaces x) populationPlaces) llines
-  let places = finalMap populationPlaces llines
-  let exists = H.lookup "501" places
-  putStrLn $ show exists
-  --putStrLn population
-  where
-    populationMap :: HashMap String Place
-    populationMap = H.empty
-    places :: HashMap String Place
-    places = H.empty
+  handle <- readFile "alice30.txt"
+  let wordlist = concatMap (\x -> words x) $ lines handle
+  let bigrams = insertBigrams wordlist H.empty
+  putStrLn "Enter number of words to print out: "
+  getAnswer <- getLine
+  let num = read getAnswer :: Int
+  g <- R.newStdGen
+  let (firstword,_) = pick (H.keys bigrams) g
+  printText num g firstword bigrams
--- Place.hs
+++ Place.hs
@@ -2,30 +2,6 @@
 module Place where
 
 import Prelude
-import Data.Kind
-
-{-
-data Place = Place { zipCodeP :: String
-                    , townP :: String
-                    , stateP :: String }
-                    deriving (Eq, Show, Read, Ord)
-
-data LocatedPlace = LocatedPlace { zipCodeLP :: String
-                                  , townLP :: String
-                                  , stateLP :: String
-                                  , latLP :: Float
-                                  , lonLP :: Float }
-                                  deriving (Eq, Show, Read, Ord)
-
-data PopulatedPlace = PopulatedPlace { zipCodePP :: String
-                                  , townPP :: String
-                                  , statePP :: String
-                                  , latPP :: Float
-                                  , lonPP :: Float
-                                  , populationPP :: Int }
-                                  deriving (Eq, Show, Read, Ord)
--}
-
 
 data Place where
   Place :: String -> String -> String -> Place
